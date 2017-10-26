d84cc4f422b6ab2df7210ec824dfd7d8d6dff650
LvZhenDong1993 <dong.lv@magic-beans.cn>
2017-10-26 16:15:37 +0800

    commit

--- app/src/main/java/com/egr/drillinghelper/ui/activity/GalleryActivity.java
+++ app/src/main/java/com/egr/drillinghelper/ui/activity/GalleryActivity.java
@@ -63,7 +63,7 @@ public class GalleryActivity extends BaseActivity {
 
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
-                View view = View.inflate(container.getContext(), R.layout.pager_item, null);
+                View view = View.inflate(container.getContext(), R.layout.item_pager, null);
                 ImageView imageView = (ImageView) view.findViewById(R.id.item_img);
                 TextView tv = (TextView) view.findViewById(R.id.tv_number);
                 tv.setText((position + 1) + "/" + getCount());
