1d79194fc7bbbf70d202223dc250a8eed6cf6443
lvzhendong <lvzhendong1993@gmail.com>
2017-11-14 11:08:04 +0800

    touch Gallery

--- app/src/main/java/com/egr/drillinghelper/ui/activity/GalleryActivity.java
+++ app/src/main/java/com/egr/drillinghelper/ui/activity/GalleryActivity.java
@@ -3,20 +3,15 @@ package com.egr.drillinghelper.ui.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.egr.drillinghelper.R;
 import com.egr.drillinghelper.ui.base.BaseActivity;
-import com.egr.drillinghelper.utils.GlideUtils;
-import com.github.ybq.parallaxviewpager.ParallaxViewPager;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
+import ru.truba.touchgallery.GalleryWidget.GalleryViewPager;
+import ru.truba.touchgallery.GalleryWidget.UrlPagerAdapter;
 
 /**
  * author lzd
@@ -26,8 +21,7 @@ import butterknife.BindView;
 
 public class GalleryActivity extends BaseActivity {
     @BindView(R.id.viewpager)
-    ParallaxViewPager viewpager;
-
+    GalleryViewPager viewpager;
     private ArrayList<String> images;
 
     public static void start(Context context, ArrayList<String> imgList) {
@@ -49,36 +43,37 @@ public class GalleryActivity extends BaseActivity {
     }
 
     private void initViewPager() {
-        PagerAdapter adapter = new PagerAdapter() {
-
-            @Override
-            public boolean isViewFromObject(View arg0, Object arg1) {
-                return arg0 == arg1;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position,
-                                    Object obj) {
-                container.removeView((View) obj);
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                View view = View.inflate(container.getContext(), R.layout.item_pager, null);
-                ImageView imageView = (ImageView) view.findViewById(R.id.item_img);
-                TextView tv = (TextView) view.findViewById(R.id.tv_number);
-                tv.setText((position + 1) + "/" + getCount());
-                GlideUtils.load(images.get(position), imageView);
-                container.addView(view, ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT);
-                return view;
-            }
-
-            @Override
-            public int getCount() {
-                return images.size();
-            }
-        };
-        viewpager.setAdapter(adapter);
+//        PagerAdapter adapter = new PagerAdapter() {
+//
+//            @Override
+//            public boolean isViewFromObject(View arg0, Object arg1) {
+//                return arg0 == arg1;
+//            }
+//
+//            @Override
+//            public void destroyItem(ViewGroup container, int position,
+//                                    Object obj) {
+//                container.removeView((View) obj);
+//            }
+//
+//            @Override
+//            public Object instantiateItem(ViewGroup container, int position) {
+//                View view = View.inflate(container.getContext(), R.layout.item_pager, null);
+//                ImageView imageView = (ImageView) view.findViewById(R.id.item_img);
+//                TextView tv = (TextView) view.findViewById(R.id.tv_number);
+//                tv.setText((position + 1) + "/" + getCount());
+//                GlideUtils.load(images.get(position), imageView);
+//                container.addView(view, ViewGroup.LayoutParams.MATCH_PARENT,
+//                        ViewGroup.LayoutParams.MATCH_PARENT);
+//                return view;
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return images.size();
+//            }
+//        };
+        UrlPagerAdapter pagerAdapter=new UrlPagerAdapter(this,images);
+        viewpager.setAdapter(pagerAdapter);
     }
 }
